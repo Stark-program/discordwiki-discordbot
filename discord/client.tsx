395cbc83cc2a35dcf08eb7163326a1eed298d7fe
Chad Stark <chad.d.stark@hotmail.com>
2022-08-08 13:19:16 -0700

    moved all discord bot related files to this repo

---
+++ discord/client.tsx
@@ -0,0 +1,9 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+module.exports = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
+});
