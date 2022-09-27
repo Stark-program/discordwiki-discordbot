f57e7bc9f50fc20aae874d021d26f3659295e64c
Chad Stark <chad.d.stark@hotmail.com>
2022-09-26 21:23:15 -0700

    added api folder and moved index.tsx to it

--- discord/startup.tsx
+++ discord/startup.tsx
@@ -1,4 +1,4 @@
-import {discordLogin} from "./bot" 
-import {expressServer} from "./data/index"
-const bot = discordLogin()
-const server = expressServer()
+import { discordLogin } from "./bot";
+import { expressServer } from "../api/index";
+const bot = discordLogin();
+const server = expressServer();
